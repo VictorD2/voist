c3e20c8475cfdf4ce170148eab90882793b6d893
Victor Hernandez <victor-2027@hotmail.com>
2023-10-29 17:36:25 -0500

    api flask

--- src/app/shared/utils/api.ts
+++ src/app/shared/utils/api.ts
@@ -1,3 +1,3 @@
 export const API = "https://voist-be.onrender.com/api/v1";
 export const DOMAIN = "https://voist-be.onrender.com";
-export const FLASK_API = "http://192.168.1.24:4001"
+export const FLASK_API = "https://voist-be-flask.onrender.com"
